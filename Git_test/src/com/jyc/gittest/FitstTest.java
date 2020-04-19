9e37be1895647c98c07e11a32d4ce0d58737b38d
root <root@163.com>
2020-04-19 18:14:57 +0800

    正常提交Java工程代码

---
+++ Git_test/src/com/jyc/gittest/FitstTest.java
@@ -0,0 +1,16 @@
+package com.jyc.gittest;
+
+/**
+ * 描述:
+ * git test
+ *
+ * @author 景远超
+ * @create 2020-04-19 18:01
+ */
+public class FitstTest {
+
+    public static void main(String[] args) {
+        System.out.println("git   test....");
+
+    }
+}
